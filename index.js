473894a1a57c88c1f2fe3d7f71b253259cd81b1b
najmul-rusu <nhrusu@gmail.com>
2022-05-24 18:06:11 +0600

    Create a server side

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config;
+const app = express();
+const port =process.env.PORT || 5000;
+
+// Middleware.
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('EveDiva Server Side!');
+})
+
+app.listen(port, () => {
+  console.log(`EveDiva listening on port ${port}`);
+})
