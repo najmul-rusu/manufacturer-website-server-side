5ad2c049d4bb961f9c85c06ea946791c7076a6eb
najmul-rusu <nhrusu@gmail.com>
2022-05-24 19:07:55 +0600

    added fake data in database also loded data

--- index.js
+++ index.js
@@ -9,14 +9,25 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//? Database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q0vic.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//? Server Stablish
 async function run() {
     try {
       await client.connect();
-      console.log('Database Connected');
+
+      const productCollection = client.db('evediva_manufacturer').collection('products');
+
+      //? get all product
+      app.get('/product', async (req, res) => {
+        const query = {};
+        const cursor = productCollection.find(query);
+        const Products = await cursor.toArray();
+        res.send(Products);
+    });
       
 
 
