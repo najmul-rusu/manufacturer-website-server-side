af08401eebb3024d0b6f60ecbc08795aea06f7af
najmul-rusu <nhrusu@gmail.com>
2022-05-30 07:32:44 +0600

    done

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ async function run() {
   }) */
 
 
-    app.put('/user/:email',verifyJWT, async (req, res) => {
+    app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -157,7 +157,7 @@ async function run() {
 
 
 
-  app.get('/admin/:email',verifyJWT, async (req, res) => {
+  app.get('/admin/:email', async (req, res) => {
     const email = req.params.email;
     const user = await userCollection.findOne({ email: email });
     const isAdmin = user.role === 'admin';
